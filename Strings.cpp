63de78e6c941a5d8b09b77874b46dc70c2d6d3a3
Andre Alvarez <andre.alvarez@ucsp.edu.pe>
2021-11-24 13:10:06 -0500

    Update Strings.cpp

--- Strings.cpp
+++ Strings.cpp
@@ -5,19 +5,21 @@ using namespace std;
 
 int main()
 {
-    string a, b,c;
+    string a, b, c;
 
     cin >> a;
     cin >> b;
 
-    cout << a.size() <<" " << b.size()<< endl;
-    
-    c = a + b;
-    cout << c<<endl;
+    cout << a.size() << " " << b.size() << endl;
 
-    
-    for (int i = c.size()-2; i > 0; i--)
-        cout << c[i];
-    
-    cout << " "<< c[0] <<c[c.size()-1];
+    c = a + b;
+    cout << c << endl;
+
+    char d;
+    d = a[0];
+    a[0] = b[0];
+    b[0] = d;
+
+    cout << a << " " << b;
+
 }
